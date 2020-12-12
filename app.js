dff32db46084c25c577fe15980eb25abd40cd175
mw18 <67554339+mw18@users.noreply.github.com>
2020-12-11 21:03:12 -0600

    pass employee, engineer, intern, and manager tests

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const employees = [{}, {}, {}]
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -17,9 +21,11 @@ const render = require("./lib/htmlRenderer");
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
+const html = render (employees);
 // After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
+// returned from the `render` function. 
+
+//Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
@@ -33,3 +39,13 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+//Ask manager prompts => then you want to build team 
+Do you want to add a team memenr => no/render html 
+yes/ name id email 
+role / engineer 
+github => new Engineer 
+school => new Intern 
+Intern 
+
+
